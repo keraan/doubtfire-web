be831e6ef2ab5e1f57295f6da408df1424ae0b1e
Kieren <kierenhuynh88@gmail.com>
2024-05-31 19:48:46 +1000

    refactor: abtracted logic to a general local storage service

--- src/app/api/services/recently-interacted-task.service.ts
+++ src/app/api/services/recently-interacted-task.service.ts
@@ -1,42 +1,62 @@
 import {Injectable} from '@angular/core';
-import {RxStorage} from 'ngx-reactive-storage';
+import { LocalStorageService } from './local-storage.service';
+
+interface RecentlyInteractedTask {
+  task_id: number;
+  date: number;
+}
+
+interface RecentlyInteractedTasks {
+  [unitId: number]: RecentlyInteractedTask[];
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecentlyInteractedTaskService {
-  private storage = new RxStorage();
+  private recentlyInteractedTasks: RecentlyInteractedTasks = {};
 
-  constructor() {
-    this.storage.get('recentlyInteractedTasks').then((data) => {
-      if (data) {
-        console.log(JSON.parse(data));
-        this.recentlyInteractedTasks = JSON.parse(data);
-      }
-    });
+  constructor(
+    private localStorageService: LocalStorageService,
+  ) {
+    this.onInit()
   }
 
-  private recentlyInteractedTasks = [];
+  private async onInit() {
+    const data = await this.localStorageService.getItem('recentlyInteractedTasks')
+    if (data && typeof data === 'object') this.recentlyInteractedTasks = data;
+  }
 
-  // TODO: Add unit id to the task id
   public addInteractedTask(taskId: number, unitId: number): void {
     const dateStamp = new Date().getTime();
-    if (!this.recentlyInteractedTasks.includes(taskId)) {
-      this.recentlyInteractedTasks.push({task_id: taskId, date: dateStamp});
+    const recentTask = {task_id: taskId, date: dateStamp}
+    // If the unit's key doesn't exist: create it.
+    if (!this.recentlyInteractedTasks[unitId]) {
+      this.recentlyInteractedTasks[unitId] = [recentTask]
       this.syncTaskIdsToLocalStorage();
+    } else {
+      const unit = this.recentlyInteractedTasks[unitId];
+      if (!unit.some((task) => task.task_id === taskId)) {
+        unit.push(recentTask)
+        this.syncTaskIdsToLocalStorage();
+      }
     }
   }
 
-  public clearRecentlyInteractedTasks(): void {
-    this.recentlyInteractedTasks = [];
+  public clearAllRecentlyInteractedTasks(): void {
+    this.recentlyInteractedTasks = {};
     this.syncTaskIdsToLocalStorage();
   }
 
-  public getRecentlyInteractedTasksIDs(): number[] {
+  public getRecentlyInteractedTasksIdsByUnitId(unitId): RecentlyInteractedTask[] {
+    return this.recentlyInteractedTasks[unitId];
+  }
+
+  public getAllRecentlyInteractedTasksIds(): RecentlyInteractedTasks {
     return this.recentlyInteractedTasks;
   }
 
   private syncTaskIdsToLocalStorage(): void {
-    this.storage.set('recentlyInteractedTasks', this.recentlyInteractedTasks);
+    this.localStorageService.setItem('recentlyInteractedTasks', this.recentlyInteractedTasks)
   }
 }
